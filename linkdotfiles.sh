bf155696470b548c8df8cd2795b4c29ceca84749
Mikko Lehto <mslehto@iki.fi>
2015-11-24 02:20:22 +0200

    makes dotfile links

---
+++ linkdotfiles.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+FILES='tmux.conf vimrc xsession'
+DOTDIR=$(expr ~/dotfiles)
+
+if [ ! -d ${DOTDIR} ];then
+	echo "DOTDIR ${DOTDIR} does not exist, bye"
+	exit 1;
+fi
+
+for file in ${FILES};do
+	if [ -f ${file} ];then
+		dotfile=$(expr ~/.${file})
+		if [ -f ${dotfile} ]; then
+			if [ -L ${dotfile} ]; then
+				echo "skipping ${dotfile} - symbolic"
+			else
+				echo "skipping ${dotfile} - already exists"
+			fi
+		else
+			cmd=$(expr "ln -s ${DOTDIR}/${file} ${dotfile}")
+			echo ${cmd}
+			eval ${cmd}
+		fi
+	fi
+done
