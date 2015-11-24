90aca94ff4eeaae5ffdb6223d62f3bd683b28aba
Mikko Lehto <mslehto@noc.setera.fi>
2015-11-24 04:37:00 +0200

    Openbox now works

--- linkdotfiles.sh
+++ linkdotfiles.sh
@@ -11,12 +11,9 @@ fi
 for file in ${FILES};do
 	if [ -f ${file} ];then
 		dotfile=$(expr ~/.${file})
-		if [ -f ${dotfile} ]; then
-			if [ -L ${dotfile} ]; then
-				echo "skipping ${dotfile} - symbolic"
-			else
-				echo "skipping ${dotfile} - already exists"
-			fi
+		if [ -L ${dotfile} ] || [ -f ${dotfile} ]; then
+			echo "skipping ${dotfile} - symbolic"
+			echo "skipping ${dotfile} - already exists"
 		else
 			cmd=$(expr "ln -s ${DOTDIR}/${file} ${dotfile}")
 			echo ${cmd}
@@ -27,15 +24,22 @@ done
 
 
 OPENBOXCONF=$(expr ~/.config/openbox)
-if [ -d ${OPENBOXCONF} ];then
-	for file in rc.xml menu.xml
+if [ ! -d ${OPENBOXCONF} ];then
+	echo "${OPENBOXCONF} does not exist"
+	echo "maybe mkdir -p ${OPENBOXCONF} first?"
+	exit 1
+else
+	for f in rc.xml menu.xml
 	do
-		f=$(expr ${OPENBOXCONF}/${file})
-	if [ -f ${f} ];then
-		echo "skipping ${f} - already exists"
+		masterfile=$(expr ${DOTDIR}/config/openbox/${f})
+		file=$(expr ${OPENBOXCONF}/${f})
+		if [ -L ${file} ] || [ -f ${file} ] ;then
+			echo "skipping ${file} - already exists"
+		else
+			dotfile=$(expr ${file})
+			cmd=$(expr "ln -s ${masterfile} ${dotfile}")
+			echo ${cmd}
+			eval ${cmd}
 	fi
 	done
-else
-	echo "${OPENBOXCONF} does not exist"
-	echo "maybe mkdir -p ${OPENBOXCONF} ?"
 fi
