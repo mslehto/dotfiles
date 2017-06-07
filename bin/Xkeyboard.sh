37258e6a9d34f5f0294746c25e29fa7044e568bf
Mikko Lehto <mslehto@iki.fi>
2017-06-07 21:45:11 +0300

    Xkeyboard.sh - disable CapsLock

--- bin/Xkeyboard.sh
+++ bin/Xkeyboard.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 setxkbmap -option
-setxkbmap -layout fi,us -option grp:shifts_toggle -option grp_led:scroll -option caps:swapescape
+setxkbmap -layout fi,us -option grp:shifts_toggle -option grp_led:scroll -option caps:escape
 xmodmap ~/dotfiles/Xmodmap
